91772d0c81db0a6154909fbed4531636adc6b4fd
NicholasLin718 <nlin311@gmail.com>
2022-05-18 10:43:26 -0400

    added calendar numbers

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calendar from './calendar/Calendar';
+
+function App() {
+  return (
+    <div>
+        <h1>app</h1>
+        <Calendar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/calendar/Calendar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import moment from 'moment';
+import "./Calendar.css";
+
+export default function Calendar() {
+    const value = moment();
+    const startDay = value.clone().startOf("month").startOf("week");
+    const endDay = value.clone().endOf("month").endOf("week");
+    const day = startDay.clone().subtract(1, "day");
+    const calendar = [];
+
+    while(day.isBefore(endDay, "day")){
+        calendar.push(Array(7)
+            .fill(0)
+            .map(() => day.add(1, "day").clone()));
+
+    }
+    return (
+
+        <div className="calendar">
+            {calendar.map(week => 
+                <div >
+                    {week.map(day => 
+                        <div className="day">
+                            {day.format("D")}
+                        </div>)}
+                </div>)}
+        </div>
+    )
+}
