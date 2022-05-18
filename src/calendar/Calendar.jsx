0d042d17878acfbc5698b1c85fc70c90ab29ac11
NicholasLin718 <nlin311@gmail.com>
2022-05-18 17:15:19 -0400

    added click effect to calendar

--- src/calendar/Calendar.jsx
+++ src/calendar/Calendar.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import moment from 'moment';
 import "./Calendar.css";
 
 export default function Calendar() {
-    const value = moment();
-    const startDay = value.clone().startOf("month").startOf("week");
-    const endDay = value.clone().endOf("month").endOf("week");
-    const day = startDay.clone().subtract(1, "day");
-    const calendar = [];
+    const [calendar, setCalendar] = useState([]);
+    const [selectedDay, setSelectedDay] = useState(moment());
 
-    while(day.isBefore(endDay, "day")){
-        calendar.push(Array(7)
-            .fill(0)
-            .map(() => day.add(1, "day").clone()));
-
-    }
+    const startDay = selectedDay.clone().startOf("month").startOf("week");
+    const endDay = selectedDay.clone().endOf("month").endOf("week");
+    
+    useEffect(() => {
+        const day = startDay.clone().subtract(1, "day");
+        const tempArray = [];
+        while(day.isBefore(endDay, "day")){
+            tempArray.push(Array(7)
+                .fill(0)
+                .map(() => day.add(1, "day").clone()));
+        }
+        setCalendar(tempArray);
+    }, [selectedDay]);
     return (
 
         <div className="calendar">
             {calendar.map(week => 
                 <div >
-                    {week.map(day => 
-                        <div className="day">
-                            {day.format("D")}
+                    {week.map(day =>
+                        <div className="day" onClick={() => setSelectedDay(day)}>
+                            <div className={selectedDay.isSame(day, "day") ? "selected" : ""}>
+                                {day.format("D")}
+                            </div>
                         </div>)}
                 </div>)}
         </div>
