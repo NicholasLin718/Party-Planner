7d5b56610c3f1fd3272f305d72547f82d653061c
Brian Zhang <bz88keys@gmail.com>
2022-05-25 23:08:14 -0400

    add some routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const express = require('express');
+
+const app = express();
+app.listen(5000);
+
+app.get('/', (req, res)=>{
+    res.send("hello");
+})
+
+app.get('/pages/:id', (req, res)=>{
+    const id = req.params.id;
+    console.log(id);
+    res.send(id);
+})
