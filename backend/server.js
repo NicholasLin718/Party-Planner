30c4cdddba31e4564a6da2c8a59028aca9edc4b5
NicholasLin718 <nlin311@gmail.com>
2022-05-25 23:08:15 -0400

    added express routing

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const express = require('express');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res)=> {
+    res.send("pog");
+})
+app.get('/pages/:id', (req, res)=>{
+    const id = req.params.id;
+    res.send(id);
+    console.log(id);
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
