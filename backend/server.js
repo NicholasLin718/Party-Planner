d6adb3d01a90255400eff6a789a37882875cd9d6
Kristy <kristy.lau@uwaterloo.ca>
2022-05-28 19:59:26 -0400

    connect to .env file through dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
     res.send("pog");
 })
-app.get('/pages/:id', (req, res)=>{
+
+app.get('/pages/:id', (req, res) => {
     const id = req.params.id;
     res.send(id);
     console.log(id);
