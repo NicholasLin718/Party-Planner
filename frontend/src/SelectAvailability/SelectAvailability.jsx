8cf0c336b33ec5728fa23d88c9fa8565dc4320a6
NicholasLin718 <nlin311@gmail.com>
2022-06-01 00:29:32 -0400

    added select availability feature (no deselect yet)

---
+++ frontend/src/SelectAvailability/SelectAvailability.jsx
@@ -0,0 +1,79 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { boxesIntersect, useSelectionContainer } from "react-drag-to-select";
+import "./styles.css"
+const MouseSelection = React.memo(({ onSelectionChange }) => {
+  const { DragSelection } = useSelectionContainer({
+    eventsElement: document.getElementById("root"),
+    onSelectionChange,
+    onSelectionStart: () => {
+      console.log("OnSelectionStart");
+    },
+    onSelectionEnd: () => console.log("OnSelectionEnd")
+  });
+
+  return <DragSelection />;
+});
+
+const SelectAvailability = () => {
+  const [selectionBox, setSelectionBox] = useState();
+  const [selectedIndexes, setSelectedIndexes] = useState([]);
+  const [selectedTotalBoxes, setSelectedTotalBoxes] = useState([]);
+  const selectableItems = useRef([]);
+
+  useEffect(() => {
+    const elementsContainer = document.getElementById("elements-container");
+    if (elementsContainer) {
+      Array.from(elementsContainer.childNodes).forEach((item) => {
+        const { left, top, width, height } = item.getBoundingClientRect();
+        selectableItems.current.push({
+          left,
+          top,
+          width,
+          height
+        });
+      });
+    }
+  }, []);
+
+  const handleSelectionChange = useCallback(
+    (box) => {
+      setSelectionBox(box);
+      const indexesToSelect = selectedIndexes;
+      selectableItems.current.forEach((item, index) => {
+        if (boxesIntersect(box, item)) {
+            console.log("this box intersects");
+          indexesToSelect.push(index);
+        }
+      });
+      console.log(indexesToSelect);
+      setSelectedIndexes(indexesToSelect);
+    },
+    [selectableItems]
+  );
+
+  return (
+    <div className="container">
+      <MouseSelection onSelectionChange={handleSelectionChange} />
+      <div id="elements-container" className="elements-container">
+        {Array.from({ length: 16 }, (_, i) => (
+          <div
+            key={i}
+            className={`element ${
+              selectedIndexes.includes(i) ? "selected" : ""
+            } `}
+          />
+        ))}
+      </div>
+
+      <div className="selection-box-info">
+        Selection Box:
+        <div>top: {selectionBox?.top || ""}</div>
+        <div>left: {selectionBox?.left || ""}</div>
+        <div>width: {selectionBox?.width || ""}</div>
+        <div>height: {selectionBox?.height || ""}</div>
+      </div>
+    </div>
+  );
+};
+
+export default SelectAvailability;
