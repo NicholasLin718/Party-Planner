e0e0c7ca63c0bd4fa6702bd40ce8a792d792b992
Kristy <kristy.lau@uwaterloo.ca>
2022-05-31 23:50:27 -0400

    commented CRUD functionality

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,34 +1,67 @@
 const Page = require('../models/model');
 
-const createPage = (req, res)=>{
-    console.log(req.body);
-    if(!req.body || !req.body.code){
-        res.status(400);
-        throw new Error('body has missing values');
-    }
-    const newPage = new Page({code: req.body.code});
-    newPage.save()
-        .then(
-            (result)=>res.send(result)
-        ).catch(
-            (err) =>{
-                console.log(err)
-                res.send('error');
-            }
-        );    
-}
-
+// @desc Get page
+// @route GET /pages/:id
+// @access Public
 const getPage = (req, res) => {
     const code = req.params.id;
-    Page.findOne({code: code})
-        .then( ()=>{
+    Page.findOne({ code: code })
+        .then(() => {
             res.send(code);
         })
-        .catch( (err)=>{
+        .catch((err) => {
             console.log(err);
         });
 }
 
+// @desc Create page
+// @route POST /pages/create
+// @access Public
+const createPage = (req, res) => {
+    console.log(req.body);
+    if (!req.body || !req.body.code) {
+        res.status(400);
+        throw new Error('body has missing values');
+    }
+    const newPage = new Page({ code: req.body.code });
+    newPage.save()
+        .then(
+            (result) => res.send(result)
+        ).catch(
+            (err) => {
+                console.log(err)
+                res.send('error');
+            }
+        );
+}
+
+// @desc Update page
+// @route PUT /pages/:id
+// @access Public
+const updatePage = (req, res) => {
+    console.log(req.body);
+    if (!req.body || !req.body.code) {
+        res.status(400);
+        throw new Error('body has missing values');
+    }
+    res.status(200).json({ code: req.body.code })
+}
+
+// @desc Delete page
+// @route DELETE /pages/:id
+// @access Private
+const deletePage = (req, res) => {
+    console.log(req.body);
+    if (!req.body || !req.body.code) {
+        res.status(400);
+        throw new Error('body has missing values');
+    }
+    res.status(200).json({ code: req.body.code })
+}
+
 module.exports = {
-    getPage, createPage
+    getPage,
+    createPage,
+    updatePage,
+    deletePage
 }
