46a255c2802610a333109416715ad623a61ccd4a
Kristy <kristy.lau@uwaterloo.ca>
2022-05-31 23:51:22 -0400

    added routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const Page = require('../models/model');
-const {getPage, createPage} = require('../controllers/controller')
+const { getPage, createPage, updatePage, deletePage } = require('../controllers/controller')
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("pog");
-})
+router.route('/pages/:id').get(getPage).put(updatePage).delete(deletePage);
+router.route('/pages/create').post(createPage);
 
-router.get('/pages/:id', getPage);
-router.post('/pages/create', createPage);
 module.exports = router;
