d0097e78396dc0aa6c537f8c0f6bac731219e395
NicholasLin718 <nlin311@gmail.com>
2022-06-01 23:32:24 -0400

    selectionboxes

--- frontend/src/SelectAvailability/MouseSelection.jsx
+++ frontend/src/SelectAvailability/MouseSelection.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 import { useSelectionContainer } from "react-drag-to-select";
 
-const MouseSelection = ({ onSelectionChange }) => {
-  const { DragSelection } = useSelectionContainer({
-    eventsElement: document.getElementById("root"),
-    onSelectionChange,
-    onSelectionStart: () => {
-      console.log("OnSelectionStart");
-    },
-    onSelectionEnd: () => console.log("OnSelectionEnd")
-  });
+// const MouseSelection = ({ onSelectionChange }) => {
+//   const { DragSelection } = useSelectionContainer({
+//     eventsElement: document.getElementById("root"),
+//     onSelectionChange,
+//     onSelectionStart: () => {
+//       console.log("OnSelectionStart");
+//     },
+//     onSelectionEnd: () => console.log("OnSelectionEnd")
+//   });
 
-  return <DragSelection />;
-};
+//   return <DragSelection />;
+// };
+
+// export default React.memo(MouseSelection);
+
+const MouseSelection = ({ onSelectionChange, selectedIndexes, selectedTotalBoxes, setSelectedTotalBoxes }) => {
+    const { DragSelection } = useSelectionContainer({
+      eventsElement: document.getElementById("root"),
+      onSelectionChange,
+      onSelectionStart: () => {
+        console.log("OnSelectionStart");
+      },
+      onSelectionEnd: () => {
+          console.log("OnSelectionEnd")
+        //   console.log(selectedIndexes);
+        //   setSelectedTotalBoxes(selectedIndexes);
+        //   console.log(selectedTotalBoxes);
+        
+      }
+    });
+  
+    return <DragSelection />;
+  };
 
 export default React.memo(MouseSelection);
