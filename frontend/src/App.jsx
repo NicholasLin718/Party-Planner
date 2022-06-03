cc1ad5ca652b6ac928a09707541ecb5daf097743
Brian Zhang <bz88keys@gmail.com>
2022-06-02 22:27:03 -0400

    delete some files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Calendar from './calendar/Calendar';
-import DragSelector from './DragSelector/DragSelector';
-import TimeSelector from './TimeSelector/TimeSelector';
-
+// import DragSelector from './DragSelector/DragSelector';
+// import TimeSelector from './TimeSelector/TimeSelector';
+import TimeSlot from './timetable/timeSlot';
+import Timetable from './timetable/timetable';
+import SelectAvailability from './SelectAvailability/SelectAvailability'
 function App() {
   return (
     <div>
         <h1>app</h1>
         {/* <Calendar/> */}
         {/* <TimeSelector/> */}
-        <DragSelector/>
+        {/*<DragSelector/>*/}
+        <SelectAvailability/>
     </div>
   );
 }
