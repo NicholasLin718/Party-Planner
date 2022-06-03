c99b3d771cb0cf84aac6629ee82ab58e1ed16d32
NicholasLin718 <nlin311@gmail.com>
2022-06-02 23:05:22 -0400

    commit explosion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Calendar from './calendar/Calendar';
-// import DragSelector from './DragSelector/DragSelector';
-// import TimeSelector from './TimeSelector/TimeSelector';
-// import SelectAvailability from './SelectAvailability/SelectAvailability';
 import Index from './AvailabilitySelector/Index';
 
 function App() {
