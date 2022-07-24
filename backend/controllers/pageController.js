ffc9b164ed9732e9a7e2d6cb3f06cb2239630fdd
Brian Zhang <bz88keys@gmail.com>
2022-07-24 00:41:59 -0400

    fix create page async bug

--- backend/controllers/pageController.js
+++ backend/controllers/pageController.js
@@ -21,10 +21,13 @@ class PageController {
             res.status(400);
             throw new Error('Body has missing values');
         }
+        try{
+            const createdPage = await Page.create(req.body);
+            res.status(200).json(createdPage);
+        } catch(error){
+            console.log(error);
 
-        const createdPage = await Page.create(req.body);
-
-        res.status(200).json(createdPage);
+        }
     };
 
     // @desc Update page
