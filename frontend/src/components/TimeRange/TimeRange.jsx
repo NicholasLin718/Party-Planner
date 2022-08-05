ad0362e06ec2542437fe1212bb8928adf7b0352e
NicholasLin718 <nlin311@gmail.com>
2022-08-05 16:56:43 -0400

    cleaned up code

--- frontend/src/components/TimeRange/TimeRange.jsx
+++ frontend/src/components/TimeRange/TimeRange.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useImperativeHandle, forwardRef } from 'react';
 import TimezoneSelect from 'react-timezone-select';
-import TimePicker from 'react-time-range';
+import TimePicker from './TimePicker/index';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 import {
@@ -80,7 +80,7 @@ const TimeRange = forwardRef((props, ref) => {
     };
 
     return (
-        <div className='App mt-12'>
+        <div className='App mt-8'>
             <form className='max-w-[1300px] m-auto min-w-[300px]'>
                 <CreatePageTextBoxComponent
                     onChangeHandler={onTitleFieldChanged}
@@ -93,20 +93,22 @@ const TimeRange = forwardRef((props, ref) => {
                     labelText={"What's The Event About?"}
                 />
             </form>
-            <div className='mt-5 min-w-[300px]'>
-                <TimezoneSelect
-                    value={selectedTimezone}
-                    onChange={setSelectedTimezone}
-                    className='m-auto w-[80%] sm:w-[80%] md:w-[75%] lg:w-[70%] xl:w-[60%] 2xl:w-[50%] 3xl:w-[40%] font-mono text-sm sm:text-base'
-                />
+            <div className='flex max-w-[1300px] m-auto min-w-[400px] '>
                 <TimePicker
-                    className='timepicker m-auto w-[80%] sm:w-[80%] md:w-[75%] lg:w-[70%] xl:w-[60%] 2xl:w-[50%] 3xl:w-[40%] font-mono text-sm sm:text-base'
+                    className='font-mono text-sm sm:text-base'
                     label='Time Picker'
                     onStartTimeChange={returnFunctionStart}
                     onEndTimeChange={returnFunctionEnd}
                     startMoment={startTime}
                     endMoment={endTime}
                 />
+                <div className='mt-[4.5px] w-[1000px]'>
+                    <TimezoneSelect
+                        value={selectedTimezone}
+                        onChange={setSelectedTimezone}
+                        className='font-mono text-sm sm:text-base w-[85%] sm:w-[85%] md:w-[80%] lg:w-[75%] xl:w-[70%] 2xl:w-[65%] 3xl:w-[60%]'
+                    />
+                </div>
             </div>
         </div>
     );
