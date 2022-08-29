84f0f242b18d9e659ad8def01095471c5b2c115f
Kristy <kristy.lau@uwaterloo.ca>
2022-08-29 13:28:06 -0400

    Styled options in poll page

--- frontend/src/pages/PollPage/NewPoll.jsx
+++ frontend/src/pages/PollPage/NewPoll.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faPersonWalkingArrowRight,
-    faXmarkCircle
+    faXmark
 } from '@fortawesome/free-solid-svg-icons';
 import e from 'cors';
 
@@ -62,7 +62,7 @@ const NewPoll = () => {
     };
 
     return (
-        <div className='flex justify-center mb-4 bg-blue-100 border-2 rounded-md shadow-md w-[600px] h-auto px-4 py-4'>
+        <div className='flex justify-center mb-4 bg-blue-100 rounded-md shadow-md w-[600px] h-auto px-4 py-4'>
             <form
                 className='flex flex-col w-[100%] sm:w-[500px]'
                 onSubmit={postPoll}>
@@ -87,13 +87,13 @@ const NewPoll = () => {
                             placeholder={'Option ' + (i + 1)}
                         />
                         <FontAwesomeIcon
-                            icon={faXmarkCircle}
-                            className='ml-2'
+                            icon={faXmark}
+                            className='absolute right-16 py-4 fa-lg text-gray-800 cursor-pointer'
                             onClick={(e) => deleteOption(e, i)}
                         />
                     </div>
                 ))}
-                <button className='flex text-rose-500' onClick={addOption}>
+                <button className='flex text-gray-800' onClick={addOption}>
                     + Add Option
                 </button>
                 <div className='flex justify-center'>
