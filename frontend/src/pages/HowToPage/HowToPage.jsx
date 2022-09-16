2c6eabf0f95bb897300363bea8627ad19f61430c
vas057 <va.max2012@gmail.com>
2022-09-16 09:21:36 -0400

    added how-to page instructions

---
+++ frontend/src/pages/HowToPage/HowToPage.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const HowToPage = () => {
+    return (
+        <div>
+            <h1>Using Party Planner!</h1>
+            <div>
+                <h3>
+                    Create a room for your event on the homepage. Share this
+                    room code with attendees to allow them to sign up and fill
+                    in their availabilities
+                </h3>
+                <h3>
+                    Enter in the event details, and select the dates to host the
+                    event
+                </h3>
+                <h3>
+                    Navigate to the event homepage to view event details and any
+                    attendees who've signed up
+                </h3>
+                <h3>
+                    Under the 'Select Availability' page, attendees will be able
+                    to enter their availabilities, and view other attendees'
+                    selections as well
+                </h3>
+                <h3>
+                    Under the 'Polls' page, you can create polls to ask
+                    attendees questions
+                </h3>
+                <h3>
+                    Under the 'Tasks' page, you can create and assign tasks to
+                    attendees
+                </h3>
+            </div>
+        </div>
+    );
+};
+
+export default HowToPage;
