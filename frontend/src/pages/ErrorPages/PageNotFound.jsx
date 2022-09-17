f78fe591169b569f5619e8e6ceca958418a1085f
NicholasLin718 <nlin311@gmail.com>
2022-09-16 20:11:07 -0400

    Merge branch 'main' of https://github.com/NicholasLin718/party-planner

--- frontend/src/pages/ErrorPages/PageNotFound.jsx
+++ frontend/src/pages/ErrorPages/PageNotFound.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import errorIcon from './404-icon.png';
-
+import { useNavigate } from 'react-router-dom';
 const PageNotFound = () => {
-    var topMargin = '20px';
+    const navigate = useNavigate();
     return (
-        <div className='flex flex-col justify-center items-center'>
-            <h1 className='font-mono mt-6 mb-4 text-5xl'>
-                <b>Page not found</b>
-            </h1>
-            <h3 className='mt-4 text-xl font-mono mb-10'>
-                Oops! The page you were looking for doesn't exist...
-            </h3>
-            <div className='self-center mt-6'>
-                <img
-                    src={errorIcon}
-                    alt='confused alien standing icon'
-                    style={{ height: '50vh' }}
-                    className='object-contain'></img>
+        <div className='flex h-screen'>
+            <div className='flex flex-col justify-center items-center align m-auto'>
+                <h3 className='text-3xl font-mono font-semibold mb-10 text-center'>
+                    Oops! The page you were looking for doesn't exist...
+                </h3>
+                <div className='self-center mt-6'>
+                    <img
+                        src={errorIcon}
+                        alt='confused alien standing icon'
+                        style={{ height: '400px' }}
+                        className='object-contain'></img>
+                </div>
+                <div
+                    className='mt-6 w-auto inline-flex hover:underline hover:cursor-pointer'
+                    onClick={() => navigate('/')}>
+                    <span className='font-mono font-semibold text-xl'>
+                        Back to home page
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            stroke-width='1.5'
+                            stroke='currentColor'
+                            className='ml-1 relative w-5 h-5 inline-flex self-center'>
+                            <path
+                                stroke-linecap='round'
+                                stroke-linejoin='round'
+                                d='M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3'
+                            />
+                        </svg>
+                    </span>
+                </div>
             </div>
         </div>
     );
